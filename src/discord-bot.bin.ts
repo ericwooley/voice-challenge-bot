5a178ec3786c6f926216b10edf8de01a33d10269
Eric Wooley <ericwooley@gmail.com>
2024-12-13 16:31:51 -0700

    add sqlite

--- src/discord-bot.bin.ts
+++ src/discord-bot.bin.ts
@@ -1,8 +1,28 @@
 import { config } from 'dotenv'
 import { ChannelType, Client as DiscordClient, Events, GatewayIntentBits } from 'discord.js'
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 config()
 
-async function listAllChannels(client: DiscordClient) {
+async function initializeDatabase() {
+  const db = await open({
+    filename: './voice-challenge-bot.db',
+    driver: sqlite3.Database,
+  })
+
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS voice_channels (
+      id TEXT PRIMARY KEY,
+      guild_id TEXT NOT NULL,
+      name TEXT NOT NULL,
+      crontab TEXT
+    )
+  `)
+
+  return db
+}
+
+async function listAllChannels(client: DiscordClient, db: any) {
   const guilds = await client.guilds.fetch()
 
   for (const guild of guilds.values()) {
@@ -10,22 +30,30 @@ async function listAllChannels(client: DiscordClient) {
     console.log(`Guild: ${fullGuild.name}`)
 
     const channels = await fullGuild.channels.fetch()
-    channels.forEach((channel) => {
-      if (channel?.type !== ChannelType.GuildText) return
+    channels.forEach(async (channel) => {
+      if (channel?.type !== ChannelType.GuildVoice) return
       console.log(`Channel Name: ${channel.name}, Type: ${channel.type}, id: ${channel.id}`)
+      // await db.run(
+      //   `
+      //   INSERT OR IGNORE INTO voice_channels (id, guild_id, name)
+      //   VALUES (?, ?, ?)
+      // `,
+      //   [channel.id, guild.id, channel.name]
+      // )
     })
   }
 }
+
 async function main() {
   const client = new DiscordClient({
-    intents: [
-
-    ] as const,
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates] as const,
   })
+
+  const db = await initializeDatabase()
 
   client.on(Events.ClientReady, () => {
     console.log(`Logged in as ${client.user?.tag}!`)
-    listAllChannels(client).catch(console.error)
+    listAllChannels(client, db).catch(console.error)
   })
 
   // // keep nodejs alive
